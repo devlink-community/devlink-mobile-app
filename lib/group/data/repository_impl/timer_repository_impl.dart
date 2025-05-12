472d0afc9fcaa0fccbf2a948e63c3a9732ff6f03
KangJiWon <doggodking@gmail.com>
2025-05-12 14:04:42 +0900

    fix(groupTimer): TimerRepositoryImpl에서 presentation 의존성 제거

--- lib/group/data/repository_impl/timer_repository_impl.dart
+++ lib/group/data/repository_impl/timer_repository_impl.dart
@@ -4,7 +4,6 @@ import 'package:devlink_mobile_app/group/data/mapper/timer_mapper.dart';
 import 'package:devlink_mobile_app/group/domain/model/member_timer.dart';
 import 'package:devlink_mobile_app/group/domain/model/timer_session.dart';
 import 'package:devlink_mobile_app/group/domain/repository/timer_repository.dart';
-import 'package:devlink_mobile_app/group/presentation/group_timer/group_timer_state.dart';
 
 class TimerRepositoryImpl implements TimerRepository {
   final TimerDataSource _dataSource;
